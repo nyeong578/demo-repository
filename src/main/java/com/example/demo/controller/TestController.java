8dac906cebb486a5c0002b3693c33379a1d39b15
Kim Eun Yeong <iris578@naver.com>
2023-06-18 02:43:17 +0900

    백엔드 개발 14. ResponseEntity로 HTTP Status 조작하기

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,8 @@ import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.TestRequestBodyDTO;
 
 import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @RestController
 @RequestMapping("test")
@@ -33,5 +36,23 @@ public class TestController {
 		list.add("See you!");
 		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
 		return response;
+	}
+	
+	@GetMapping("/testResponseEntityOk")
+	public ResponseEntity<?>testControllerResponseEntityOk(){
+		List<String> list = new ArrayList<String>();
+		list.add("Hello World! I'm ResponseEntity. And you get 200!");
+		list.add("See you!");
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		return ResponseEntity.ok().body(response);
+	}
+	
+	@GetMapping("/testResponseEntityBad")
+	public ResponseEntity<?>testControllerResponseEntityBad(){
+		List<String> list = new ArrayList<String>();
+		list.add("Hello World! I'm ResponseEntity. And you get 400!");
+		list.add("See you!");
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		return ResponseEntity.badRequest().body(response);
 	}
 }
